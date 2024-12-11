891d4ba9f26dfeb93938c35ca65fcefcbf5f4c5f
Pavani-70 <pavanipadigela06@gmail.com>
2024-12-11 16:11:39 +0530

    addition

---
+++ addition.java
@@ -0,0 +1,14 @@
+public class addition{
+public void static main(String args[])
+{
+if(args.length<2){
+System.out.println("please provide two numbers as parameters");
+return;
+}
+int num1-Integer.perseInt(args[0]);
+int num2=Integer.perseInt(args[1]);
+int sum=num1+num2;
+System.ou.println("The sum of"+bum1+"and"+num2+"is"+sum);
+}
+}
+
