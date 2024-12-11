d291c487ba0e4ebbaf535d44c75e6222afd659f0
Pavani-70 <pavanipadigela06@gmail.com>
2024-12-11 16:21:38 +0530

    Update addition.java

--- addition.java
+++ addition.java
@@ -5,10 +5,10 @@ if(args.length<2){
 System.out.println("please provide two numbers as parameters");
 return;
 }
-int num1=Integer.perseInt(args[0]);
-int num2=Integer.perseInt(args[1]);
+int num1=Integer.parseInt(args[0]);
+int num2=Integer.parseInt(args[1]);
 int sum=num1+num2;
-System.ou.println("The sum of"+bum1+"and"+num2+"is"+sum);
+System.ou.println("The sum of"+num1+"and"+num2+"is"+sum);
 }
 }
 
